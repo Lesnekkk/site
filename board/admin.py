950d4bfba78b5bb2e71b76ee79041d3f74ca6f94
Nikita Lesnichiy <lesnekkpm@mail.ru>
2023-03-09 20:09:14 +0300

    'Someadminpanelchanges'

--- board/admin.py
+++ board/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 from .models import Bb
+from .models import Rubric
+admin.site.register(Rubric)
 class Bbadmin(admin.ModelAdmin):
-    list_display = ('title','contentt','price', 'published')
+    list_display = ('title','contentt','price', 'published', 'rubric')
     list_display_links = ('title','contentt')
     search_fields = ('title', 'contentt')
 admin.site.register(Bb, Bbadmin)
